cfa4f627c35f14fd4593fbbf5e651017b288d672
Saul Pereira <saul_pereira@hotmail.com>
2024-04-30 17:41:47 -0600

    88. Working with Handlebars
    88. Working with Handlebars

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const exp = require('constants');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
